15182fb93ca7f85b265467d09151c78fcc65dac9
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-11 00:31:58 +0100

    add: discount calculator

--- JS/main.js
+++ JS/main.js
@@ -18,13 +18,31 @@ enter.addEventListener('click', function(){
     console.log(userKm)
 
     //calcolare il prezzo base del biglietto (0.21 per km)
-    const startPrice=userKm*0.21;
-    console.log(startPrice)
+    let price=userKm*0.21;
+    console.log(price)
 
     //input fascia d'età
     const userAge=userAgeInput.value;
     console.log(userAge)
-    
+
+    ticketPrice.innerText=price
+
+    /*calcolare sconto:
+
+        se il passeggero è minorenne applicare sconto del 20%
+        se il passeggero è over 65 applicare sconto del 40% 
+
+    */
+   if(userAge=='minorenne'){
+    const discountPerc=20;
+    price=price-price*discountPerc/100;
+    console.log(price)
+   }
+   if(userAge=='over65'){
+    const discountPerc=40;
+    price=price-price*discountPerc/100;
+    console.log(price)
+   }
 
     
 })
