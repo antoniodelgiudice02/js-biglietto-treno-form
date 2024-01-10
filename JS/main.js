0bc0b93a0ec149bf6634cca29fb32f7b1284d808
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-10 16:26:37 +0100

    add: sales

--- JS/main.js
+++ JS/main.js
@@ -7,11 +7,22 @@ const resultPrice = document.getElementById('ticket-price')
 
 
 button.addEventListener('click', function(){
+
     const basePrice = userKm.value*0.21;
     console.log(basePrice)
 
     const age = userAge.value;
     console.log(age)
+
+    if(age < 18){
+        const resultPrice = basePrice-basePrice*20/100;
+        console.log(resultPrice)
+    }
+
+    if(age >= 65){
+        const resultPrice = basePrice-basePrice*40/100;
+        console.log(resultPrice)
+    }
 
     
 })
